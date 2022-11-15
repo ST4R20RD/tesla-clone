030f4b5e9004682e12d03747331df0cb20f9555d
ST4R20RD <goncalo.estrelado@hotmail.com>
2022-11-15 19:51:34 +0000

    Added Fade animations to Section Component

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,18 +1,23 @@
 import React from "react";
 import styled from "styled-components";
+import Fade from "react-reveal/Fade";
 
 function Section({ title, description, backgroundImg, leftBtnText, rightBtnText }) {
   return (
     <Wrap bgImage={backgroundImg}>
-      <ItemText>
-        <h1>{title}</h1>
-        <p>{description}</p>
-      </ItemText>
+      <Fade bottom>
+        <ItemText>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </ItemText>
+      </Fade>
       <Buttons>
-        <ButtonGroup>
-          <LeftButton>{leftBtnText}</LeftButton>
-          {rightBtnText ? <RightButton>{rightBtnText}</RightButton> : null}
-        </ButtonGroup>
+        <Fade bottom>
+          <ButtonGroup>
+            <LeftButton>{leftBtnText}</LeftButton>
+            {rightBtnText ? <RightButton>{rightBtnText}</RightButton> : null}
+          </ButtonGroup>
+        </Fade>
         <DownArrow src="/images/down-arrow.svg" />
       </Buttons>
     </Wrap>
